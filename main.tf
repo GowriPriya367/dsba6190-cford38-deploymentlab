cf50323325fc68b79ffca074994449b1df8a0dd5
GowriPriya367 <akulagowri24@gmail.com>
2025-12-03 14:25:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,11 +34,50 @@ resource "azurerm_resource_group" "rg" {
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "sto${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  name                     = "sto${var.class_name}${var.student_name}${var.environment}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  tags = local.tags
+}
+
+// -----------------------------
+// Azure SQL Server
+// -----------------------------
+resource "azurerm_sql_server" "sql_server" {
+  name                         = "sqlsrv-${var.class_name}-${var.student_name}-${var.environment}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
+
+  tags = local.tags
+}
+
+// -----------------------------
+// Azure SQL Database
+// -----------------------------
+resource "azurerm_sql_database" "sql_db" {
+  name                = "sqldb-${var.class_name}-${var.student_name}-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.sql_server.name
+  sku_name            = "S0"
+
+  tags = local.tags
+}
+
+// -----------------------------
+// Virtual Network for VM
+// -----------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   tags = local.tags
 }
