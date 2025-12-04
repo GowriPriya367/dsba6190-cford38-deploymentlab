cc465643e64686a7400ed3fe5a2abdb89676df43
GowriPriya367 <akulagowri24@gmail.com>
2025-12-03 21:12:28 -0500

    add storage account network rules for vnet-only access

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
+// -----------------------------------------------------------------------------
 // Tags
+// -----------------------------------------------------------------------------
 locals {
   tags = {
     class      = var.tag_class
@@ -7,22 +9,17 @@ locals {
   }
 }
 
-// Existing Resources
-
-/// Subscription ID
-
-# data "azurerm_subscription" "current" {
-# }
-
+// -----------------------------------------------------------------------------
 // Random Suffix Generator
-
+// -----------------------------------------------------------------------------
 resource "random_integer" "deployment_id_suffix" {
   min = 100
   max = 999
 }
 
+// -----------------------------------------------------------------------------
 // Resource Group
-
+// -----------------------------------------------------------------------------
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
   location = var.location
@@ -30,9 +27,9 @@ resource "azurerm_resource_group" "rg" {
   tags = local.tags
 }
 
-
+// -----------------------------------------------------------------------------
 // Storage Account
-
+// -----------------------------------------------------------------------------
 resource "azurerm_storage_account" "storage" {
   name                     = "sto${var.class_name}${var.student_name}${var.environment}"
   resource_group_name      = azurerm_resource_group.rg.name
@@ -43,36 +40,41 @@ resource "azurerm_storage_account" "storage" {
   tags = local.tags
 }
 
-// -----------------------------
-// Azure SQL Server
-// -----------------------------
-resource "azurerm_sql_server" "sql_server" {
-  name                         = "sqlsrv-${var.class_name}-${var.student_name}-${var.environment}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
+// -----------------------------------------------------------------------------
+// Azure SQL Server (MSSQL)
+// -----------------------------------------------------------------------------
+resource "azurerm_mssql_server" "sql_server" {
+  name                = "sqlsrv-${var.class_name}-${var.student_name}-${var.environment}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  version             = "12.0"
+
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
 
-  tags = local.tags
-}
-
-// -----------------------------
-// Azure SQL Database
-// -----------------------------
-resource "azurerm_sql_database" "sql_db" {
-  name                = "sqldb-${var.class_name}-${var.student_name}-${var.environment}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sql_server.name
-  sku_name            = "S0"
+  minimum_tls_version = "1.2"
 
   tags = local.tags
 }
 
-// -----------------------------
-// Virtual Network for VM
-// -----------------------------
+// -----------------------------------------------------------------------------
+// Azure SQL Database (MSSQL)
+// -----------------------------------------------------------------------------
+resource "azurerm_mssql_database" "sql_db" {
+  name = "sqldb-${var.class_name}-${var.student_name}-${var.environment}"
+
+  # Correct way for MSSQL databases
+  server_id = azurerm_mssql_server.sql_server.id
+
+  sku_name    = "S0"
+  max_size_gb = 10
+
+  tags = local.tags
+}
+
+// -----------------------------------------------------------------------------
+// Virtual Network
+// -----------------------------------------------------------------------------
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.class_name}-${var.student_name}-${var.environment}"
   address_space       = ["10.0.0.0/16"]
@@ -81,5 +83,3 @@ resource "azurerm_virtual_network" "vnet" {
 
   tags = local.tags
 }
-
-##
